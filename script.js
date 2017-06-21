ca944ed6054848feb2db18479f8e33c85277710e
Przemek <p.ostrowinski@gmail.com>
2017-06-21 13:26:37 +0200

    add simply quote generator

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    
+    var prefix = "https://cors-anywhere.herokuapp.com/",
+        tweetLink = "https://twitter.com/intent/tweet?text=",
+        quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+    
+    $.ajaxSetup({ cache: false });
+    
+    function getQuote() {
+        $.getJSON(prefix + quoteUrl, createTweet);
+    }
+    
+    function createTweet(input) {
+        var data = input[0],
+            quoteText = $(data.content).text().trim(),
+            quoteAuthor = data.title,
+            tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+        if (!quoteAuthor.length) {
+            quoteAuthor = "Unknown author";
+        }
+        
+        if (quoteText.length > 140) {
+            getQuote();
+        } else {
+            var tweet = tweetLink + encodeURIComponent(tweetText);
+            $('.quote').text(quoteText);
+            $('.author').text("Author: " + quoteAuthor);
+            $('.tweet').attr('href', tweet);
+        }
+    }
+    
+    $('.trigger').click(function () {
+        getQuote();
+    });
+});
